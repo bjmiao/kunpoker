3c9911b3b88bf33846fdff3cd7d1a39369d68b0e
bjmiao <bjmiao501@live.cn>
2021-01-15 00:56:58 +0800

    add a foolish ALL_IN robot;

---
+++ Texaspoker/modules/texaspoker/AI/v1_01.py
@@ -0,0 +1,11 @@
+'''
+    AI: v1_1版本
+    详见AI-v1.1_interpretation.txt
+'''
+from lib.client_lib import Decision
+import random
+
+def ai(id, state):
+    decision = Decision()
+    decision.allin = 1
+    return decision
